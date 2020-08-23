5c3770be769342478019cb96d3a6889678f4e61a
okapeeokapee <okapee420@gmail.com>
2020-08-23 19:07:18 +0900

    Change probability(Already hidden)

--- js/main.js
+++ js/main.js
@@ -5,9 +5,18 @@
 
   btn.addEventListener('click',()=>{
     const results = ['大吉','中吉','凶','いたよし','にっしー'];
+    btn.textContent=results[Math.floor(Math.random()*results.length)];
+  }
+    // const n=Math.random();
+    // if(n<0.05){
+    //   btn.textContent='大吉';
+    // }else if(n<0.2){
+    //   btn.textContent='中吉';
+    // }else{
+    //   btn.textContent='凶';
+    // const results = ['大吉','大吉','大吉','大吉','にっしー'];
     // const n =Math.floor(Math.random()*results.length);
     // btn.textContent=results[n];
-    btn.textContent=results[Math.floor(Math.random()*results.length)];
     // btn.textContent = n;
     // switch(n){
     //   case 0:
@@ -19,5 +28,4 @@
     //   case 2:
     //     btn.textContent='凶';
     // }
-  });
-}
+  )};
